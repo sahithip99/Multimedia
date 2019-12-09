a975bab68be73bbe1fbd131ea84837464fd68ca7
unknown <sasahithi@gmail.com>
2019-12-09 15:08:20 -0500

    updating song table

--- main.sql
+++ main.sql
@@ -1,21 +1,21 @@
-
 CREATE TABLE MP3_File(
 	File_ID INT not null Primary Key,
 	File_Size INT,
 	FOREIGN KEY (File_ID) references MP3_File(File_ID));
-	
+
 CREATE TABLE Song(
-	Song_ID INT not null,
+	Song_ID INT not null Primary Key,
 	Album_ID INT not null,
 	File_ID INT,
 	Feat_Singer_ID INT,
-	Song_Title VARCHAR(25),
+	Song_Title VARCHAR(50),
 	Song_Length INT,
 	Genre VARCHAR(25),
 	Download_Permit VARCHAR(25),
-	PRIMARY KEY(Song_ID, Album_ID),
+    Singer_ID INT,
 	FOREIGN KEY (File_ID) references MP3_File(File_ID),
-	FOREIGN KEY (Feat_Singer_ID) references Featured_Artist(Feat_Singer_ID)
+	FOREIGN KEY (Feat_Singer_ID) references Featured_Artist(Feat_Singer_ID),
+	FOREIGN KEY (Singer_ID) references Singer(Singer_ID)
 	);
 	CREATE TABLE Album(
 	Album_ID INT not null PRIMARY KEY,
@@ -79,16 +79,23 @@ CREATE TABLE Song(
     INSERT into Featured_Artist(Feat_Singer_ID) values(8800);
     INSERT into Featured_Artist(Feat_Singer_ID) values(7000);
     
-    
-    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2234','8776','Hey Brother',4.37,'Pop','Permit','0001',null);
-    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2344','9876','Dancing Queen',2.30,'Pop','Permit','0002',8800);
-    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3336','3736','Rock and Roll',5.45,'Rock','Permit','0003',null);
-    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3000','8773','DanceFloor',6.00,'Dance','Permit','0004',7000);
-    
     INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('9004','Avicii','Bergling','32','1');
     INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('7008','Sarah','Jane','12','3');
     INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('7804','Elvis','Presley','100','8');
     INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('5674','DJ','Beat','22','3');
+    INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('7078','Mariah','Carey','60','5');
+    INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('6768','DJ','Beat','22','3');
+    
+    
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID,Singer_ID) values('2234','8776','Hey Brother',4.37,'Pop','Permit','0001',null);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID,Singer_ID) values('2344','9876','Dancing Queen',2.30,'Pop','Permit','0002',8800);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID,Singer_ID) values('3336','3736','Rock and Roll',5.45,'Rock','Permit','0003',null);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID,Singer_ID) values('3000','8773','DanceFloor',6.00,'Dance','Permit','0004',7000);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID,Singer_ID) values('2000','6660','All I Want For Christmas is You',3.00,'Pop','Yes','0005',null);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID,Singer_ID) values('1000','6630','Bohemian Rapsody',3.20,'Rock','No','0006',null);
+    
+
+
     
     INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('9004','2234','5006');
     INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7008','2344','5007');
