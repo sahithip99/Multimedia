fa1a8f18c75f9ce8046a4ff21ee19ebc2ec9b5f1
unknown <sasahithi@gmail.com>
2019-12-08 21:50:10 -0500

    insert

--- main.sql
+++ main.sql
@@ -44,7 +44,8 @@ CREATE TABLE Song(
 	FOREIGN KEY (Album_ID) references Album(Album_ID));
 	
 	CREATE TABLE Featured_Artist(
-	Producer_ID INT not null PRIMARY KEY);
+    Feat_Singer_ID INT not null PRIMARY KEY);
+	
 	
 	CREATE TABLE CD_Image(
 	CD_ID INT not null,
@@ -70,6 +71,32 @@ CREATE TABLE Song(
 	Primary key (Song_ID, Singer_ID, Album_ID),
 	FOREIGN KEY (Singer_ID) references Singer(Singer_ID)
 	);
+	INSERT into MP3_File(File_ID,File_Size) values('0001','32');
+	INSERT into MP3_File(File_ID,File_Size) values('0002','30');
+    INSERT into MP3_File(File_ID,File_Size) values('0003','54');
+    INSERT into MP3_File(File_ID,File_Size) values('0004','67');
+    
+    INSERT into Featured_Artist(Feat_Singer_ID) values(8800);
+    INSERT into Featured_Artist(Feat_Singer_ID) values(7000);
+    
+    
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2234','8776','Hey Brother',4.37,'Pop','Permit','0001',null);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2344','9876','Dancing Queen',2.30,'Pop','Permit','0002',8800);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3336','3736','Rock and Roll',5.45,'Rock','Permit','0003',null);
+    INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3000','8773','DanceFloor',6.00,'Dance','Permit','0004',7000);
+    
+    INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('9004','Avicii','Bergling','32','1');
+    INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('7008','Sarah','Jane','12','3');
+    INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('7804','Elvis','Presley','100','8');
+    INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('5674','DJ','Beat','22','3');
+    
+    INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('9004','2234','5006');
+    INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7008','2344','5007');
+    INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7804','3736','5008');
+    INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('5674','8773','5009');
+    
+    
+    
 
 
 	
