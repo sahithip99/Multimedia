38e02420510f9f45dbeefc1585c45c379d0808ca
sahithip99 <36111446+sahithip99@users.noreply.github.com>
2019-12-09 22:35:36 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -58,30 +58,6 @@ CREATE TABLE MP3_File(
 	File_Size INT);
 
 
-ALTER TABLE Song
-	ADD FOREIGN KEY (File_ID) REFERENCES MP3_File(File_ID);
-ALTER TABLE Song
-	ADD FOREIGN KEY (Album_ID) REFERENCES Album(Album_ID);
-ALTER TABLE Song
-	ADD FOREIGN KEY (Feat_Singer_ID) REFERENCES Featured_Artist(Feat_Singer_ID);
-
-ALTER TABLE SongBy
-	ADD FOREIGN KEY (Singer_ID) REFERENCES Singer(Singer_ID);
-ALTER TABLE SongBy
-	ADD FOREIGN KEY (Song_ID) REFERENCES Song(Song_ID);
-
-
-ALTER TABLE Album
-	ADD FOREIGN KEY (Image_ID) REFERENCES CD_Image(Image_ID);
-ALTER TABLE Album
-	ADD FOREIGN KEY (Singer_ID) REFERENCES Singer(Singer_ID);
-
-ALTER TABLE Album
-	ADD FOREIGN KEY (Genre_ID) REFERENCES Genre(Genre_ID);
-ALTER TABLE Album
-	ADD FOREIGN KEY (Producer_ID) REFERENCES Producer(Producer_ID);
-
-
 
 
 INSERT into MP3_File(File_ID,File_Size) values('0001','32');
@@ -109,33 +85,135 @@ INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num
 INSERT into Singer(Singer_ID,Singer_FName,Singer_LName,Total_Num_Songs,Total_Num_Albums) values('1250','Shawn','Mendes','4','1');
     
     
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2234','8776','Hey Brother','4.37','Pop','True','0001',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2344','9876','Dancing Queen',2.30,'Pop','True','0002',8800);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3336','3736','Rock and Roll',5.45,'Rock','True','0003',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3000','8773','DanceFloor',6.00,'Dance','False','0004',7000);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2000','6660','Silent Night',3.00,'Pop','True','0005',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1000','6630','Bohemian Rapsody',3.20,'Rock','False,'0006',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2019','6660','O Holy Night',4.00,'Pop','True','0007',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1300','6830','Bad Guy',3.40,'Rap','True','0008','5002');
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1560','6830','Goodbyes',2.00,'Rap','False','0009',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1450','6830','Bury a Friend',6.00,'Rap','True','0010',null);
-INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1250','6830','Senorita',2.30,'Rap','True','0011',null);
-    
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('9004','2234','8776');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7008','2234','5006');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7804','2234','3637');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('5674','2234','9876');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7078','2234','6660');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('6768','2234','5006');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('6000','2234','5006');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('6001','2234','6830');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('5300','2344','5007');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('1250','3736','5008');
-INSERT into SongBy(Singer_ID,Song_ID,Album_ID) values('7078','8773','5009'); 
-    
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2234','8776','Hey Brother','4.37','Pop','Permit','0001',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2344','9876','Dancing Queen',2.30,'Pop','Permit','0002',8800);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3336','3736','Rock and Roll',5.45,'Rock','Permit','0003',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('3000','8773','Dance Beats',6.00,'Dance','Permit','0004',7000);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2000','6660','Silent Night',3.00,'Pop','Permit','0005',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1000','6630','Bohemian Rapsody',3.20,'Rock','No','0006',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('2019','6660','O Holy Night',4.00,'Pop','Permit','0007',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1300','6830','Bad Guy',3.40,'Rap','No','0008','5002');
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1560','6830','Goodbyes',2.00,'Rap','No','0009',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1450','6830','Bury a Friend',6.00,'Rap','Permit','0010',null);
+INSERT into Song(Song_ID,Album_ID,Song_Title,Song_Length,Genre,Download_Permit,File_ID,Feat_Singer_ID) values('1250','6830','Senorita',2.30,'Rap','Permit','0011',null);
+ 
+INSERT ALL
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('9004','2234','8776')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('5674','2344','9876')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('7804','3336','3736')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('6768','3000','8773')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('7078','2000','6660')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('6000','1000','6630')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('7078','2019','6660')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('6001','1300','6830')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('5300','1560','6830')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('6001','1450','6830')
+	INTO Songby(Singer_ID, Song_ID, Album_ID)
+	Values('1250','1250','6830')
+Select * from DUAL;
+
 INSERT into Featured_Artist(Feat_Singer_ID,Feat_Artist_FName,Feat_Artist_LName) values('8800','Benny','Anderson');
 INSERT into Featured_Artist(Feat_Singer_ID,Feat_Artist_FName,Feat_Artist_LName) values('5002','Keke','Palmer');
 INSERT into Featured_Artist(Feat_Singer_ID,Feat_Artist_FName,Feat_Artist_LName) values('7000','DJ','Dots');
+    
+
+INSERT ALL
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES ('6660','7078','0001','0728','8001','Merry Christmas', '9.45', DATE '1994-11-01','2','Permit')
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES ('6830','6001','0002', '0729', '8002','Spotify Top Hits of 2019 Album', '16.57', DATE '2019-12-09', '4','Permit')
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES ('8776','9004','0003','0728','8003', 'True', '3.35', DATE '2013-09-13','1','Permit')
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES ('9876','5674','0004','0728','8006','Dancing Queen','5.43',DATE '2018-08-28','1','Permit')
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES ('3736','7804','0005', '0730','8004','Always on My Mind','3.58',DATE '1972-03-29','1','No')
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES('8773','5674','0006','0731','8007','Dance Beats','4.12',DATE '2014-05-13','1','Permit')
+	INTO Album(Album_ID, Singer_ID, Image_ID, Genre_ID, Producer_ID, Album_Name, Length, Released_Date, Total_Songs, Download_permit)
+	VALUES('6630','6000','0007','0730','8005','A Night at the Opera','5.55',DATE '1975-11-21','1','Permit')
+SELECT * from DUAL;
+
+INSERT ALL 
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0001','Link to Merry Christmas Image')
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0002','Link to Spotify Top Hits Image')
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0003','Link to True Image')
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0004','Link to Dancing Queen Image')
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0005','Link to Always on my Mind Image')
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0006','Link to Dance Beats Image')
+	INTO CD_Image(image_ID,Image_Link)
+	Values ('0007','Link to A Night at the Opera Image')
+SELECT * from DUAL;
+
+INSERT ALL
+	INTO Genre(Genre_ID,Genre_Name)
+	Values('0728','Pop')
+	INTO Genre(Genre_ID,Genre_Name)
+	Values('0729','Rap')
+	INTO Genre(Genre_ID,Genre_Name)
+	Values('0730','Rock')
+	INTO Genre(Genre_ID,Genre_Name)
+	Values('0731','Dance')
+SELECT * from DUAL;
+
+INSERT ALL 
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8001','6660','Columbia Records')
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8002','6830','Spotify')
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8003','8776','Avicii Production')
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8004','3736','Sun Recordings')
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8005','6630','EMI')
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8006','9876','Cher Production')
+	INTO Producer(Producer_ID,Album_ID,Producer_Name)
+	Values('8007','8773','DJ Beat Recordings')
+SELECT * from DUAL;
 
 
-SELECT * FROM Song;
+
+
+ALTER TABLE Song
+	ADD FOREIGN KEY (File_ID) REFERENCES MP3_File(File_ID);
+ALTER TABLE Song
+	ADD FOREIGN KEY (Album_ID) REFERENCES Album(Album_ID);
+ALTER TABLE Song
+	ADD FOREIGN KEY (Feat_Singer_ID) REFERENCES Featured_Artist(Feat_Singer_ID);
+
+ALTER TABLE SongBy
+	ADD FOREIGN KEY (Singer_ID) REFERENCES Singer(Singer_ID);
+ALTER TABLE SongBy
+	ADD FOREIGN KEY (Song_ID) REFERENCES Song(Song_ID);
+
+
+ALTER TABLE Album
+	ADD FOREIGN KEY (Image_ID) REFERENCES CD_Image(Image_ID);
+ALTER TABLE Album
+	ADD FOREIGN KEY (Singer_ID) REFERENCES Singer(Singer_ID);
+
+ALTER TABLE Album
+	ADD FOREIGN KEY (Genre_ID) REFERENCES Genre(Genre_ID);
+ALTER TABLE Album
+	ADD FOREIGN KEY (Producer_ID) REFERENCES Producer(Producer_ID);
+	
+
+	
